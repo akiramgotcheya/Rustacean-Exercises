9d98ab85393e8e8dc64a59c37c0039fb86971eec
Akira MG <93864815+akiramgotcheya@users.noreply.github.com>
2022-07-29 23:09:00 +0530

    Create expression.rs

---
+++ 1. Fundamentals/expression.rs
@@ -0,0 +1,31 @@
+// Question 2
+
+// Topic: Working with expressions
+//
+// Requirements:
+// * Print "its big" if a variable is > 100
+// * Print "its small" if a variable is <= 100
+//
+// Notes:
+// * Use a boolean variable set to the result of
+//   an if..else expression to store whether the value
+//   is > 100 or <= 100
+// * Use a function to print the messages
+// * Use a match expression to determine which message
+//   to print
+
+
+fn main() {
+  let x = match_bool(100);
+  let condition = true;
+  if condition < 100 &&  > -10
+  println!("{:?}",x);
+}
+
+fn match_bool(n:i32){
+  match n {
+    
+        >100 => println!("its small"),
+        100|_ => println!("its big")
+  }
+}

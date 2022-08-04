802b12a3e3393561bbd93e99d9dd55d758268f37
Akira MG <93864815+akiramgotcheya@users.noreply.github.com>
2022-08-04 23:19:33 +0530

    Create ref1.rs

---
+++ 3. Ownership-2/ref1.rs
@@ -0,0 +1,14 @@
+// Remove something to make it work
+// Don't remove a whole line !
+fn main() {
+    let mut s = String::from("hello");
+
+    let r1 = &mut s;
+    let r2 = "world";
+
+    println!("{}, {}", r1, r2);
+
+    println!("Success!");
+}
+
+// Ln7 cannot borrow mut s more than once so changed it to &str 
